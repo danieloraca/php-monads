a6e74b64078700e4b68c36dc8ec13e5fa75542fe
Dan <daniel.oraca@gmail.com>
2024-10-05 20:57:34 +0100

    i am a monad

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php
+
+class Result
+{
+    private $value;
+    private $error;
+
+    private function __construct($value = null, $error = null)
+    {
+    	$this->value = $value;
+    	$this->error = $error;
+    }
+
+    public static function success($value): Result
+    {
+        return new self($value);
+    }
+
+    public static function error($error): Result
+    {
+        return new self($error);
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->error === null;
+    }
+
+    public function map(callable $f): Result
+    {
+        if (!$this->isSuccess()) {
+            return $this;
+        }
+
+        return self::success($f($this->value));
+    }
+
+    public function getOrElse($default)
+    {
+        return $this->isSuccess() ? $this->value : $default;
+    }
+
+    public function getError()
+    {
+        return $this->error;
+    }
+    
+}
+
+// implementation
+
+function divide($numerator, $denominator): Result
+{
+    if ($denominator == 0) {
+        return Result::error("Cannot divide by 0");
+    }
+
+    return Result::success($numerator / $denominator);
+}
+
+$result = divide(10, 0) //error
+    ->map(function($result) {
+        return $result * 2; // this will not run due to error
+    });
+
+if ($result->isSuccess()) {
+    echo "Result: " . $result->getOrElse(0); // Shows the result if no error
+} else {
+    echo "Error: " . $result->getError();
+}
+
+// boom, monads!
+
